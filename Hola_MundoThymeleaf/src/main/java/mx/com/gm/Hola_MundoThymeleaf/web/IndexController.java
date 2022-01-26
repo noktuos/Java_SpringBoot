5b83d15a52bd72f7d64e6f8e218594e7a2026904
noktos <josuemttorres@gmail.com>
2022-01-25 17:07:36 -0700

    Seccion 5 CRUD done

--- Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/web/IndexController.java
+++ Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/web/IndexController.java
@@ -6,12 +6,15 @@ package mx.com.gm.Hola_MundoThymeleaf.web;/*
 
 import lombok.extern.slf4j.Slf4j;
 
+import mx.com.gm.Hola_MundoThymeleaf.domain.Persona;
 import mx.com.gm.Hola_MundoThymeleaf.service.PersonaService;
+import mx.com.gm.Hola_MundoThymeleaf.service.PersonaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -29,5 +32,28 @@ public class IndexController {
         model.addAttribute("personas",personas);
         return "index";
     }
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "mod";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona",persona);
+        return "mod";
+    }
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
+
 
 }
