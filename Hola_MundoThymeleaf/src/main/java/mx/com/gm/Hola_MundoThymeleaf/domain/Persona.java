ac65b2fb3c0542597423f5149e5b92019ad9790d
noktos <josuemttorres@gmail.com>
2022-01-26 13:21:01 -0700

    Section 6 - Validations done

--- Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/domain/Persona.java
+++ Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/domain/Persona.java
@@ -7,6 +7,8 @@ package mx.com.gm.Hola_MundoThymeleaf.domain;/*
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
@@ -18,9 +20,15 @@ public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPersona;
+
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
+    @Email
     private String email;
+
     private String telefono;
 
 }
--- Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/web/IndexController.java
+++ Hola_MundoThymeleaf/src/main/java/mx/com/gm/Hola_MundoThymeleaf/web/IndexController.java
@@ -13,8 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -38,7 +41,10 @@ public class IndexController {
     }
 
     @PostMapping("/guardar")
-    public String guardar(Persona persona){
+    public String guardar(@Valid Persona persona, Errors errors){
+        if(errors.hasErrors()){
+            return "mod";
+        }
         personaService.guardar(persona);
         return "redirect:/";
     }
